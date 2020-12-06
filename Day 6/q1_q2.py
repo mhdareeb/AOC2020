46e3ee82ee9de03972c9ddb592d4df5720a045c8
Mohammad Areeb <areeb.mohammad11@gmail.com>
2020-12-06 11:47:41 +0530

    Day 6 modified

--- Day 6/q1_q2.py
+++ Day 6/q1_q2.py
@@ -17,23 +17,19 @@ def intersectionLength(group):
         intersection=intersection.intersection(set(sequence))
     return len(intersection)
 
-lines=[]
 with open('../input.txt','r') as f:
     group=[]
+    unionCount=0
+    intersectionCount=0
     for line in f:
         if(line!='\n'):
             group+=[line.strip()]
         else:
-            lines+=[group]
+            unionCount+=unionLength(group)
+            intersectionCount+=intersectionLength(group)            
             group=[]
-    lines+=[group]
-    f.close()
-
-unionCount=0
-intersectionCount=0
-for group in lines:
     unionCount+=unionLength(group)
     intersectionCount+=intersectionLength(group)
-
-print(unionCount)
-print(intersectionCount)
+    print(unionCount)
+    print(intersectionCount)
+    f.close()
