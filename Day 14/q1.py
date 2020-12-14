b1e2794c952102e388c22bf2c5d8464ca43895e6
Mohammad Areeb <areeb.mohammad11@gmail.com>
2020-12-14 12:16:56 +0530

    Day 14

---
+++ Day 14/q1.py
@@ -0,0 +1,39 @@
+import os
+import sys
+
+if(os.getenv('username')=="Areeb"):
+    sys.stdin = open('../input.txt', 'r')
+    sys.stdout = open('../output.txt', 'w')
+
+def binary(x,n):
+    part=bin(x)[2:]
+    return '0'*(n-len(part))+part
+
+def mutate(mask,x):
+    n=len(mask)
+    binx=binary(x,n)
+    result=[char for char in binx]
+    for i in range(n):
+        if(mask[i]!='X'):
+            result[i]=mask[i]
+    decimal=int(''.join(result),2)
+    return decimal
+
+with open('../input.txt','r') as f:
+    total=0
+    memory={}
+    for line in f:
+        line=line.strip()
+        if(line[1]=='a'):
+            mask=line[7:]
+        else:
+            start=line.index('[')
+            end=line.index(']')
+            address=line[start+1:end]
+            original=int(line[end+4:])
+            mutated=mutate(mask,original)
+            if address in memory:
+                total-=memory[address]
+            memory[address]=mutated
+            total+=mutated
+    print(total)
---
+++ Day 14/q2.py
@@ -0,0 +1,56 @@
+import os
+import sys
+
+if(os.getenv('username')=="Areeb"):
+    sys.stdin = open('../input.txt', 'r')
+    sys.stdout = open('../output.txt', 'w')
+
+def binary(x,n):
+    part=bin(x)[2:]
+    return '0'*(n-len(part))+part
+
+def mutate(mask,x):
+    n=len(mask)
+    binx=binary(x,n)
+    result=[char for char in binx]
+    for i in range(n):
+        if(mask[i]!='0'):
+            result[i]=mask[i]
+    return result
+
+def combinations(sequence,i):
+    global mutated_addresses
+    if(i==len(sequence)):
+        decimal=int(''.join(sequence),2)
+        mutated_addresses.append(decimal)
+        return
+    if(sequence[i]=='X'):
+        sequence[i]='0'
+        combinations(sequence,i+1)
+        sequence[i]='1'
+        combinations(sequence,i+1)
+        sequence[i]='X'
+    else:
+        combinations(sequence,i+1)
+
+with open('../input.txt','r') as f:
+    total=0
+    memory={}
+    for line in f:
+        line=line.strip()
+        if(line[1]=='a'):
+            mask=line[7:]
+        else:
+            start=line.index('[')
+            end=line.index(']')
+            original=int(line[start+1:end])
+            value=int(line[end+4:])
+            mutated=mutate(mask,original)
+            mutated_addresses=[]
+            combinations(mutated,0)
+            for address in mutated_addresses:
+                if address in memory:
+                    total-=memory[address]
+                memory[address]=value
+                total+=value
+    print(total)
